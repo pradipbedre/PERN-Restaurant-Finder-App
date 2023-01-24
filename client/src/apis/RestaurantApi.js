fc9a67c83e214d87a4c3e9252658c73347e31769
Pradip Bedre <bedrepradip0@gmail.com>
2023-01-25 01:26:32 +0530

    all done

--- client/src/apis/RestaurantApi.js
+++ client/src/apis/RestaurantApi.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   /*   baseURL: process.env.SERVER, */
-  baseURL: "https://pradip-restaurants-backend.onrender.com/api/v1/restaurants",
+  baseURL: "http://localhost:3005/api/v1/restaurants",
 });
